b41e9e8d5f1e4593c82ff248233c38192104ab6e
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-01-26 21:45:57 +0100

    Convert CentralLogging to use extension registration
    Bug: T180816
    Change-Id: I8b605d480bb5503ffb2ec46672a91a3a3b11913b

--- CentralLogging.php
+++ CentralLogging.php
@@ -1,33 +1,39 @@
 <?php
-
 /**
- * Extension to allow you to log to a central wiki rather than on that wiki.
- * Implemented through the job queue
- * See README for usage instructions
+ * Extension:CentralLogging
  *
- * @author Kunal Mehta <legoktm@gmail.com>
- * @license MIT License
+ * Extension to allow you to log to a central wikirather than on that wiki.
+ * Implemented through the job queue. See README for usage instructions.
+ *
+ * Copyright (C) 2013, Kunal Mehta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
-/**
- * Database name of the wiki to log to
- * Individual extensions will be able to override this
- * but it will fallback to this setting
- */
-$wgCentralWiki = 'metawiki';
-
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'CentralLogging',
-	'author' => 'Kunal Mehta',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:CentralLogging',
-	'descriptionmsg' => 'centrallogging-desc',
-	'version' => '1.0',
-);
-
-$wgAutoloadClasses['CentralLogEntry'] = __DIR__ . '/CentralLogEntry.php';
-$wgAutoloadClasses['CentralLogFormatter'] = __DIR__ . '/CentralLogFormatter.php';
-$wgAutoloadClasses['CentralLogJob'] = __DIR__ . '/CentralLogJob.php';
-$wgJobClasses['centrallogJob'] = 'CentralLogJob';
-$wgMessagesDirs['CentralLogging'] = __DIR__ . '/i18n';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'CentralLogging' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['CentralLogging'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the CentralLogging extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the CentralLogging extension requires MediaWiki 1.25+' );
+}
